c4ce50a83e9957302e7c1ce66b2a9e906a2cdcd9
Michael Baldassaro <mbaldassaro@gmail.com>
2021-12-20 12:10:46 -0500

    added np random seed

--- example.py
+++ example.py
@@ -2,4 +2,5 @@ import pandas as pd
 import numpy as np
 data = {'Letter': ['A', 'B', 'C', 'D', 'E'], 'Number': [5, 4, 3, 2, 1]}
 df = pd.DataFrame(data) #this creates a dataframe using 'data' above containing 5 rows x 2 columns 
+np.random.seed(2022) #set random.seed(#) guarantees result will be static and replicable
 print(df.loc[np.random.choice(df.index, size=2)]) #randomly selects and prints 2 rows in the dataframe
